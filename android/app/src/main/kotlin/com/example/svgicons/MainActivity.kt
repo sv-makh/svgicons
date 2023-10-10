1647b571336501d6d9036e8439cefe01629267ce
sv-makh <mithriam@mail.ru>
2023-10-11 00:58:18 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/svgicons/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.svgicons
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/my_home_page.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_icon/path_icon.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  Future<String> fetchIconData() async {
+    final rawData = await rootBundle.loadString('assets/vector_info.json');
+    final map = jsonDecode(rawData);
+    return map['fillGeometry'][0]['path'];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('path_icon'),),
+      body: FutureBuilder<String>(
+        future: fetchIconData(),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            final iconData = PathIconData.fromData(snapshot.data!);
+            return Center(
+              child: Column(
+                children: [
+                  PathIcon(
+                    iconData,
+                    color: Colors.orange,
+                    size: 80,
+                    semanticLabel: 'icon from path',
+                  ),
+                  Text(snapshot.data!),
+                ],
+              ),
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
+      ),
+    );
+  }
+}
